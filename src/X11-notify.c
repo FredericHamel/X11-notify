06e8f7e941c342c4548580e69171c5585219450d
Frederic Hamel <frederic.hamel123@gmail.com>
2018-11-29 10:01:39 -0500

    Modified X11-notify

--- src/X11-notify.c
+++ src/X11-notify.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include <unistd.h>
 #include <X11/X.h>
@@ -23,12 +24,21 @@ typedef struct {
   GC gc;
 } Client;
 
+float get_battery_level();
+
 void notify(Client* cl, float percent) {
   char msg[128];
-  size_t len = sprintf(msg, "Charge: %0.1f%%", percent);
+  size_t len;
+
+  struct timespec start, end;
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
+  len = sprintf(msg, "Charge: %0.1f%%", percent);
   XEvent ev;
   XMapWindow(cl->dpy, cl->win);
   for(;;) {
+
     XNextEvent(cl->dpy, &ev);
     switch(ev.type) {
       case Expose:
@@ -36,6 +46,13 @@ void notify(Client* cl, float percent) {
         break;
       case ButtonPress:
         goto close;
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+      printf("Test\n");
+    if (end.tv_sec - start.tv_sec > 2) { // 2 sec
+      percent = get_battery_level();
+      len = sprintf(msg, "Charge: %0.1f%%", percent);
+      start.tv_sec = end.tv_sec;
     }
   }
 close:
@@ -95,13 +112,13 @@ float get_battery_level() {
 }
 
 int main(int argc, char **argv) {
-  const float LOW_BATTERY = 10;
+  const float LOW_BATTERY = 15;
   const float HIGH_BATTERY = 85;
   Client cl;
   float battery_level;
   for(;;) {
     battery_level = get_battery_level();
-    if (battery_level < LOW_BATTERY || battery_level > HIGH_BATTERY) {
+    if (1 || battery_level < LOW_BATTERY || battery_level > HIGH_BATTERY) {
       create_client(&cl);
       notify(&cl, battery_level);
       XDestroyWindow(cl.dpy, cl.win);
